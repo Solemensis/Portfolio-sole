7beaf6a5093608d01a946d7b8e950ac42247a66a
Ulaş Saçlı <ulassacli@outlook.com>
2022-11-22 19:40:02 +0300

    up

--- components/MainSection.vue
+++ components/MainSection.vue
@@ -113,7 +113,7 @@ function runRotate2() {
       <div class="blob blob-1">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 440.7 428.7">
           <path
-            fill="#fe2440"
+            fill="#FFBA49"
             d="M410.6 78.8c36 52.5 36.1 126 19.2 194C412.9 340.7 379 403 330 421.9c-49 19-113.1-5.3-178.6-34C85.8 359.2 18.7 326.1 3.5 276.4-11.7 226.7 25 160.3 71.7 105.3 118.3 50.3 174.8 6.8 239 .7c64.1-6 135.7 25.5 171.6 78.1z"
           ></path>
         </svg>
@@ -170,7 +170,7 @@ function runRotate2() {
 
 .blob-1 {
   animation-delay: 3000ms;
-  opacity: 0.07;
+  opacity: 0.09;
 }
 .blob-2 {
   grid-column-start: 2;
