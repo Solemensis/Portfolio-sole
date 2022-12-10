5d25dcfefd78ae0b7bed0500c12cfb7057bad54c
Ulaş Saçlı <ulassacli@outlook.com>
2022-12-10 13:41:42 +0300

    seo upds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   //   css: ["@/assets/css/blobz.min.css"],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      title: "sole - portf.",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Portfolio website of a web developer - 2022",
+        },
+      ],
+    },
+  },
 });
